69d145242bec1abfb20b4952164a4198836696f1
Kedmond24 <Kedmond24@live.com>
2023-11-10 18:42:04 -0500

    ADD variable.tf file

---
+++ initTerraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+# variable "app_vpc_id" {
+#   type = string
+#   default = aws_vpc.app_vpc.id
+# }
+# variable "public_subnet_a" {
+#   type = string
+#   default = my_pub_subnetA_id
+# }
+# variable "public_subnet_b" {
+#   type = string
+#   default = my_pub_subnetB_id
+# }
