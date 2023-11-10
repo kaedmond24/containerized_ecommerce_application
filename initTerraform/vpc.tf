effd35e00c15815a269051ce8457fdce198d395e
Kedmond24 <Kedmond24@live.com>
2023-11-10 11:48:44 -0500

    ADD auto public IP

--- initTerraform/vpc.tf
+++ initTerraform/vpc.tf
@@ -14,9 +14,10 @@ resource "aws_vpc" "app_vpc" {
 }
 
 resource "aws_subnet" "public_a" {
-  vpc_id            = aws_vpc.app_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.app_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = "true"
 
   tags = {
     "Name"    = "public | us-east-1a"
@@ -36,9 +37,10 @@ resource "aws_subnet" "private_a" {
 }
 
 resource "aws_subnet" "public_b" {
-  vpc_id            = aws_vpc.app_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
+  vpc_id                  = aws_vpc.app_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = "true"
 
   tags = {
     "Name"    = "public | us-east-1b"
