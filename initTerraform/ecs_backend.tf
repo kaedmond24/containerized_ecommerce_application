d9ea3bca13ce289d4f71b624e92778fb8a20584b
Kedmond24 <Kedmond24@live.com>
2023-11-10 16:46:00 -0500

    Moved file to root folder

---
+++ initTerraform/ecs_backend.tf
@@ -0,0 +1,81 @@
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = "us-east-1"
+
+}
+
+# Cluster
+resource "aws_ecs_cluster" "ecommerce-d8-cluster" {
+  name = "ecommerce-d8-cluster"
+  tags = {
+    Name      = "ecommerce-ecs"
+    "Project" = "deployment 8"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "log-group" {
+  name = "/ecs/ecommerce-logs"
+
+  tags = {
+    Application = "ecommerce-app"
+    "Project"   = "deployment 8"
+  }
+}
+
+###################
+# Task Definition #
+###################
+
+resource "aws_ecs_task_definition" "ecommerce-backend-task" {
+  family = "ecommerce-backend-task"
+
+  container_definitions = <<EOF
+  [
+  {
+      "name": "ecommerce-backend-container",
+      "image": "lani23/app8be:latest",
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+          "awslogs-group": "/ecs/ecommerce-logs",
+          "awslogs-region": "us-east-1",
+          "awslogs-stream-prefix": "ecs"
+        }
+      },
+      "portMappings": [
+        {
+          "containerPort": 8000
+        }
+      ]
+    }
+  ]
+  EOF
+
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = "1024"
+  cpu                      = "512"
+  execution_role_arn       = "arn:aws:iam::988716448983:role/ECSTaskExecutionRole"
+  task_role_arn            = "arn:aws:iam::988716448983:role/ECSTaskExecutionRole"
+
+}
+
+# ECS Service
+resource "aws_ecs_service" "ecommerce-backend-service" {
+  name                 = "ecommerce-backend-service"
+  cluster              = aws_ecs_cluster.ecommerce-d8-cluster.id
+  task_definition      = aws_ecs_task_definition.ecommerce-backend-task.arn
+  launch_type          = "FARGATE"
+  scheduling_strategy  = "REPLICA"
+  desired_count        = 1
+  force_new_deployment = true
+
+  network_configuration {
+    subnets = [
+      my_private_subnet_A_id
+    ]
+    assign_public_ip = false
+    security_groups  = [aws_security_group.ingress_app_backend.id]
+  }
+}
