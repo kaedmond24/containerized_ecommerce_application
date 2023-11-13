37197ab2dd06a98583483129dc6df50979215a30
Nalani Daniels <135375665+nalDaniels@users.noreply.github.com>
2023-11-13 18:37:41 -0500

    Update ecs_backend.tf

--- initTerraform/ecs_backend.tf
+++ initTerraform/ecs_backend.tf
@@ -1,12 +1,12 @@
 ############
 # Provider #
 ############
-provider "aws" {
-access_key = var.aws_access_key
-secret_key = var.aws_secret_key
-region     = "us-east-1"
+# provider "aws" {
+# access_key = var.aws_access_key
+# secret_key = var.aws_secret_key
+# region     = "us-east-1"
 
-}
+#}
 
 ##################
 # Create Cluster #
